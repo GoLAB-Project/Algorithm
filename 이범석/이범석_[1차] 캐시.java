8c77cc45350224183af6d46f44dc94cc5fa6fc40
Leebeom-seok <84014651+Leebeom-seok@users.noreply.github.com>
2023-07-19 12:11:07 +0900

    Update 이범석_[1차] 캐시.java

--- 이범석/이범석_[1차] 캐시.java
+++ 이범석/이범석_[1차] 캐시.java
@@ -2,15 +2,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-	    public int solution(int cacheSize, String[] cities) {
+	public int solution(int cacheSize, String[] cities) {
 	        int answer = 0;
-	        
 	        int citiesSize = cities.length;
 	        Queue<String> cacheQueue = new LinkedList<>();
-            if(cacheSize==0) {
-                answer = 5*citiesSize;
-                return answer;
-            }
+                if(cacheSize==0) return 5*citiesSize; // 캐쉬 사이즈가 0일때
 	        
 	        for(int i=0; i<citiesSize; i++) {
 	        	boolean isexist = cacheQueue.remove(cities[i].toLowerCase());
@@ -22,5 +18,5 @@ class Solution {
 	        	cacheQueue.add(cities[i].toLowerCase());	        	
 	        }
 	        return answer;
-	    }
 	}
+}
