b68887db4c00ac9b0c9463f23522484d0e1044fd
Leebeom-seok <84014651+Leebeom-seok@users.noreply.github.com>
2023-08-16 13:28:23 +0900

    Update and rename 이범석_두 큐 합 같게 만들기 to 이범석_두 큐 합 같게 만들기.java

--- 이범석/이범석_두 큐 합 같게 만들기.java
+++ 이범석/이범석_두 큐 합 같게 만들기.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 class Solution {
 	public int solution(int[] queue1, int[] queue2) {
-	    int answer = -1, odd = 0, max = 0;
+	    int answer = -1, max = 0;
 	    long sum = 0, sumQueue1 = 0, sumQueue2 = 0;
 	        
 	    Queue<Integer> que1 = new LinkedList<>();
@@ -12,8 +12,6 @@ class Solution {
 	    for(int i=0; i<queue1.length; i++) {
 	        if(max<queue1[i]) max = queue1[i];
 	        if(max<queue2[i]) max = queue2[i];
-	        if(queue1[i]%2 == 1) odd++;
-	        if(queue2[i]%2 == 1) odd++;
 	        sumQueue1 += queue1[i];
 	        sumQueue2 += queue2[i];
 	        	
@@ -22,18 +20,18 @@ class Solution {
 	        que2.add(queue2[i]);
 	    }
 	        
-	    if(sum % 2 == 1 || odd % 2 == 1) return answer;
-	    long targetNum = sum / 2;
-	    if(max > targetNum) return answer;
+	    if(sum%2==1) return answer;
+	    long targetNum = sum/2;
+	    if(max>targetNum) return answer;
 	        
 	    answer = 0;
 	    boolean possible = false;
-	    while(answer <= 3 * queue1.length) { // 이 횟수를 바꾸니 테스트 1이 성공으로 바뀜.. 왜인지 모름
-	        if(sumQueue1 == sumQueue2) {
+	    while(answer<=3*queue1.length) { // 이 횟수를 바꾸니 테스트 1이 성공으로 바뀜.. 왜인지 모름
+	        if(sumQueue1==sumQueue2) {
 	        	possible = true;
 	        	break;
 	        }
-	        else if (sumQueue1 > sumQueue2) {
+	        else if (sumQueue1>sumQueue2) {
 				int movedNum = que1.poll();
 				sumQueue1 -= movedNum;
 				que2.add(movedNum);
