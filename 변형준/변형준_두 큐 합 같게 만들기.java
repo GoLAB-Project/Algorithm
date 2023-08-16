03de3ff96669fab8380ab3b7d08fcfac275cb7d5
skybluelion <brotherjun96@gmail.com>
2023-08-16 12:56:40 +0900

    Update 변형준_두 큐 합 같게 만들기.java

--- 변형준/변형준_두 큐 합 같게 만들기.java
+++ 변형준/변형준_두 큐 합 같게 만들기.java
@@ -11,44 +11,45 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-		int answer = 0;
-		long q1hap, oldHap, newHap = 0;
+	int answer = 0;
+	long q1hap, oldHap, newHap = 0;
 		
-		Queue<Integer> q1 = new LinkedList<>();
-		Queue<Integer> q2 = new LinkedList<>();
+	Queue<Integer> q1 = new LinkedList<>();
+	Queue<Integer> q2 = new LinkedList<>();
         
-		q1hap = Arrays.stream(queue1).sum();
-		oldHap = q1hap + Arrays.stream(queue2).sum();
+	q1hap = Arrays.stream(queue1).sum();
+	oldHap = q1hap + Arrays.stream(queue2).sum();
 
     // 1. 각 큐의 원소 합을 같게 만들 수 없는 경우
-		if (oldHap % 2 == 1) return -1;
+	if (oldHap % 2 == 1) return -1;
         
     // 2. 각 큐의 원소 합이 같은 경우
-		newHap = oldHap / 2;
+	newHap = oldHap / 2;
         
-		for (int i = 0; i < queue1.length; i++) {
-        q1.offer(queue1[i]);
-        q2.offer(queue2[i]);
-		}
+	for (int i = 0; i < queue1.length; i++) {
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
+	}
         
     // 합이 같아질 때까지 반복
-		int temp = 0;
+	int temp = 0;
 
-		while (q1hap != newHap) {
+	while (q1hap != newHap) {
         if (q1hap < newHap) {
-			      temp = q2.poll();
-			      q1.offer(temp);
-			      q1hap += temp;
-		    } else {
-			      temp = q1.poll();
-			      q2.offer(temp);
-			      q1hap -= temp;
-		    }
-		    answer++;      	
+	    temp = q2.poll();
+	    q1.offer(temp);
+	    q1hap += temp;
+	} else {
+	    temp = q1.poll();
+	    q2.offer(temp);
+	    q1hap -= temp;
+	}
+	answer++;      	
         // 큐1, 2의 길이는 같고 2번씩 반복하면 처음과 같아지므로 불가능한 경우
         if (answer >= (queue1.length * 2) * 2) 
-        return -1;
-		}
-		return answer;
+            return -1;
+	}
+	    
+	return answer;
     }
 }
